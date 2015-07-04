e9aacbcbda19c30e8be834922112640d582fe745
elyas ghasemi <egh1374@gmail.com>
2015-07-04 18:48:01 +0430

    rm new lines in log

--- file_logger.js
+++ file_logger.js
@@ -3,6 +3,7 @@ var fs = require('fs');
 module.exports = (function(argument) {
 	return {
 		file_log: function(log) {
+			log = log.replace(/\n/g, ' ');
 			console.log(log);
 			fs.appendFile("./server.log", log + '\n', function(err) {
 				if (err) return console.log(err);
